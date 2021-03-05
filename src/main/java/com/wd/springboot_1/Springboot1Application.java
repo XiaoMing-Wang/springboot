59336de2e39f6e88e7a6b8ef91e43007e74c1fd7
wq <wq>
2021-03-05 17:03:46 +0800

    no message

--- src/main/java/com/wd/springboot_1/Springboot1Application.java
+++ src/main/java/com/wd/springboot_1/Springboot1Application.java
@@ -2,11 +2,23 @@ package com.wd.springboot_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import redis.clients.jedis.Jedis;
 
 @SpringBootApplication
 public class Springboot1Application {
 
     public static void main(String[] args) {
+
+        //Jedis使用
+        Jedis jedis = new Jedis("localhost");
+        jedis.set("aa", "123456");
+        String object = jedis.get("aa");
+        System.out.println(object);
+
+
+
+
+
         SpringApplication.run(Springboot1Application.class, args);
     }
 
